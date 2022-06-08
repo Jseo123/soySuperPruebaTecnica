ada79048fc0198f99743b27d62d4cf2aa5b0177b
jseo123 <jose123135@gmail.com>
2022-06-08 20:56:18 +0200

    Created a function to join title with the rest of the news info

--- src/crawler/crawler.js
+++ src/crawler/crawler.js
@@ -6,12 +6,30 @@ const init = async (url) => {
     uri: url,
     transform: (body) => cheerio.load(body),
   });
-  const titles = [];
-  const websiteTitle = $(".titlelink").each((i, el) => {
+  const newsArray = [];
+  const completeArray = [];
+  const websiteTitle = $(".titlelink ").each((i, el) => {
     const title = $(el).text();
-    titles.push({ i, title });
+    newsArray.push({ title });
   });
-  return titles;
+
+  //separated this function to keep init at normal size and to be order to test the different parts later.
+  await joinArrays(newsArray, completeArray, $);
+
+  return completeArray;
+};
+
+const joinArrays = async (newsArray, completeArray, $) => {
+  //because of the website layout (I hate tables), I was forced to separete website info from the title and to join the title
+  // and the rest of the info into a single array I was forced to map it when just doing a single cheerio search would have
+  //been more efficient.
+  const websiteInfo = $(".subtext").each((i, el) => {
+    const user = $(el).find(".hnuser").html();
+    newsArray.map((e) => {
+      let arrayTitle = e.title;
+      completeArray.push({ arrayTitle, user });
+    });
+  });
 };
 
 module.exports = { init };
