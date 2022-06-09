0fb3efb41b76772cc31f0365380d268d8e01b8ec
jseo123 <jose123135@gmail.com>
2022-06-09 17:31:44 +0200

    Made crawler testing to make sure it returns an array with objects

--- testing/crawler.test.js
+++ testing/crawler.test.js
@@ -1,10 +1,9 @@
-const app = require("../src/server");
-const request = require("supertest");
 const crawler = require("../src/crawler/crawler");
 
 describe("Init function at crawler", () => {
-  test("Init should return an array of type JSON", async () => {
+  test("Init should return an array of objects", async () => {
     const response = await crawler.init("https://news.ycombinator.com/");
     expect(response).toBeInstanceOf(Array);
+    expect(response[0]).toBeInstanceOf(Object);
   });
 });
